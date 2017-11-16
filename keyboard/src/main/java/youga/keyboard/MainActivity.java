7a466128f6a2a8d1f7748abdd5b60b89a095579a
YougaKing <yougakingwu@gmail.com>
2017-11-16 13:44:49 +0800

    键盘

---
+++ keyboard/src/main/java/youga/keyboard/MainActivity.java
@@ -0,0 +1,26 @@
+package youga.keyboard;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Intent intent  = new Intent(this,MoveLayoutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                break;
+        }
+    }
+}
---
+++ keyboard/src/main/java/youga/keyboard/MoveLayoutActivity.java
@@ -0,0 +1,13 @@
+package youga.keyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_layout);
+    }
+}
