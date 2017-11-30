cd4e2cb323d8d2b7aea9a160a8fa92e659cbc97c
yougakingwu <yougakingwu@gmail.com>
2017-11-30 18:00:02 +0800

    键盘

--- keyboard/src/main/java/youga/keyboard/MainActivity.java
+++ keyboard/src/main/java/youga/keyboard/MainActivity.java
@@ -1,6 +1,7 @@
 package youga.keyboard;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button1:
-                Intent intent  = new Intent(this,MoveLayoutActivity.class);
+                Intent intent = new Intent(this, MoveLayoutActivity.class);
                 startActivity(intent);
                 break;
             case R.id.button2:
+                intent = new Intent(this, LayoutActivity.class);
+                startActivity(intent);
                 break;
         }
     }
